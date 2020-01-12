6c45a4eea2d0ae2831ae87a1586c9e6a23dc7734
juliantreweeke <juliantreweeke@gmail.com>
2020-01-12 12:57:54 +1100

    yolo

--- .netlify/functions/sendinblue.js
+++ .netlify/functions/sendinblue.js
@@ -1,30 +1,33 @@
-// exports.handler = async (event, context, callback) => {
-//     console.log(event);
-//     const pass = (body) => {callback(null, {statusCode: 200, body: JSON.stringify(body)})}
-//     try {
-//       console.log('trying');
-//       let response = await fetch("https://api.sendinblue.com/v3/contacts", 
-//     {
-//      credentials: 'same-origin',
-//      method: event.httpMethod,
-//      headers: {
-//        "Content-Type": "application/json",
-//        "api-key":`${process.env.SEND_IN_BLUE_API_KEY}`,
-//     },
-//      body: event.body
-//     })
-//      let data = await response.json()
-//      console.log('sign up! ', data)
-//      await pass(data)
-//    } catch(err) {
-//      console.log('error with email sign up'. err)
-//        let error = {
-//          statusCode: err.statusCode || 500,
-//          body: JSON.stringify({error: err.message})
-//     }
-//     await pass(error)
-//    }
-//   }
+const fetch = require('node-fetch');
+
+exports.handler = async (event, context, callback) => {
+    console.log(event);
+    const pass = (body) => {callback(null, {statusCode: 200, body: JSON.stringify(body)})}
+    try {
+      console.log('trying');
+    let response = await fetch("https://api.sendinblue.com/v3/contacts", 
+    {
+     credentials: 'same-origin',
+     method: event.httpMethod,
+     headers: {
+       "Content-Type": "application/json",
+       "api-key":`${process.env.SEND_IN_BLUE_API_KEY}`,
+    },
+     body: event.body
+    })
+    console.log(response, 'response');
+     let data = await response.json()
+     console.log('sign up! ', data)
+     await pass(data)
+   } catch(err) {
+     console.log('error with email sign up'. err)
+       let error = {
+         statusCode: err.statusCode || 500,
+         body: JSON.stringify({error: err.message})
+    }
+    await pass(error)
+   }
+  }
 
 
 
@@ -85,53 +88,53 @@
      
       
   // };
-  const fetch = require('node-fetch');
-  
-  module.exports.handler = (event, context, callback) => {
-  
-      const formData = JSON.parse(event.body);
-      const email = formData.email;
-      let errorMessage = null;
-  
-      if (!formData) {
-          errorMessage = "No form data supplied";
-          console.log(errorMessage);
-          callback(errorMessage);
-      }
-  
-      if (!email) {
-          errorMessage = "No EMAIL supplied";
-          console.log(errorMessage);
-          callback(errorMessage);
-      }
-  
-      const subscriber = JSON.stringify(formData);
-      console.log("Sending data to sendinBlue", subscriber);
 
-    //   fetch(`https://api.sendinblue.com/v3/contacts`, {
-    //   method: 'POST',
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     "api-key":`${process.env.SEND_IN_BLUE_API_KEY}`,
-    //  },
-    //   body: subscriber
-    // })
-    // .then(response => callback(null, {statusCode: 200, body: JSON.stringify(response)})
-    // .catch(error => callback(error))
-    fetch('https://api.sendinblue.com/v3/contacts', {
-      method: 'POST', // or 'PUT'
-      headers: {
-        'Content-Type': 'application/json',
-        "api-key":`${process.env.SEND_IN_BLUE_API_KEY}`
-      },
-      body: subscriber,
-    })
-    .then((response) => callback(null, {statusCode: 200, body: JSON.stringify(response)})
-    .catch((error) => {
-      console.log(error)
-      callback(error);
-    }))
-  };
+  
+  // module.exports.handler = (event, context, callback) => {
+  
+  //     const formData = JSON.parse(event.body);
+  //     const email = formData.email;
+  //     let errorMessage = null;
+  
+  //     if (!formData) {
+  //         errorMessage = "No form data supplied";
+  //         console.log(errorMessage);
+  //         callback(errorMessage);
+  //     }
+  
+  //     if (!email) {
+  //         errorMessage = "No EMAIL supplied";
+  //         console.log(errorMessage);
+  //         callback(errorMessage);
+  //     }
+  
+  //     const subscriber = JSON.stringify(formData);
+  //     console.log("Sending data to sendinBlue", subscriber);
+
+  //   //   fetch(`https://api.sendinblue.com/v3/contacts`, {
+  //   //   method: 'POST',
+  //   //   headers: {
+  //   //     "Content-Type": "application/json",
+  //   //     "api-key":`${process.env.SEND_IN_BLUE_API_KEY}`,
+  //   //  },
+  //   //   body: subscriber
+  //   // })
+  //   // .then(response => callback(null, {statusCode: 200, body: JSON.stringify(response)})
+  //   // .catch(error => callback(error))
+  //   fetch('https://api.sendinblue.com/v3/contacts', {
+  //     method: 'POST', // or 'PUT'
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       "api-key":`${process.env.SEND_IN_BLUE_API_KEY}`
+  //     },
+  //     body: subscriber,
+  //   })
+  //   .then((response) => callback(null, {statusCode: 200, body: JSON.stringify(response)})
+  //   .catch((error) => {
+  //     console.log(error)
+  //     callback(error);
+  //   }))
+  // };
 
 
 
